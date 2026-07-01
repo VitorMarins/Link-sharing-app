b49ff4c2a4dfe1134d89a5f40d4965bcf47fa8f8
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-01 19:05:10 -0300

    feat(password):added password hash function

--- backend/src/utils/password.ts
+++ backend/src/utils/password.ts
@@ -1,4 +1,4 @@
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 
 const SALT_ROUNDS = 10;
 
@@ -6,6 +6,9 @@ export async function hashPassword(password: string): Promise<string> {
   return await bcryptjs.hash(password, SALT_ROUNDS);
 }
 
-export async function comparePassword(password: string, hash: string): Promise<boolean> {
+export async function comparePassword(
+  password: string,
+  hash: string
+): Promise<boolean> {
   return await bcryptjs.compare(password, hash);
 }
