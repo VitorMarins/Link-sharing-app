a305d41a4b96ff4f6299f3266b63a282d9fe8001
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-02 11:49:45 -0300

    feat(link): added LinkService

---
+++ backend/src/services/LinkService.ts
@@ -0,0 +1,49 @@
+import type { ILinksRepository, CreateLinkData, UpdateLinkData } from '../repositories/ILinkRepository';
+import { UserService } from './UserService';
+
+export class LinkService {
+    constructor(private linksRepository: ILinksRepository, private userService: UserService) {}
+
+    async create(data: CreateLinkData) {
+        await this.userService.findById(data.userId);
+        return await this.linksRepository.create({
+            ...data,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    }
+
+    async findall() {
+        return await this.linksRepository.findall();
+    }
+
+    async findById(id: string) {
+        return await this.linksRepository.findById(id);
+    }
+
+    async findByUserId(userId: string) {
+        await this.userService.findById(userId);
+        return await this.linksRepository.findByUserId(userId);
+    }
+
+    async update(id: string, data: Partial<UpdateLinkData>) {
+        const link = await this.linksRepository.findById(id);
+        if (!link) {
+            throw new Error('Link not found');
+        }
+
+        return await this.linksRepository.update(id, {
+            ...data,
+            updatedAt: new Date(),
+        });
+    
+    }
+
+    async delete(id: string) {
+        const link = await this.linksRepository.findById(id);
+        if (!link) {
+            throw new Error('Link not found');
+        }
+        return await this.linksRepository.delete(id);
+    }
+}
