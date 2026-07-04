f1baf4dbcbf50a465af4ca73ebd82a31b3e87271
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-04 01:36:00 -0300

    refactor(routes): improve code formatting for LinkRoutes and UserRoutes

--- backend/src/routes/LinkRoutes.ts
+++ backend/src/routes/LinkRoutes.ts
@@ -10,13 +10,23 @@ const linkRepository = new LinkRepository();
 const linkService = new LinkService(linkRepository, userService);
 const linkController = new LinkController(linkService);
 
-linkRoutes.post("/", authMiddleware, (req, res) => linkController.create(req, res));
-linkRoutes.get("/", authMiddleware, (req, res) => linkController.findAll(req, res));
-linkRoutes.get("/:id", authMiddleware, (req, res) => linkController.findById(req, res));
+linkRoutes.post("/", authMiddleware, (req, res) =>
+  linkController.create(req, res)
+);
+linkRoutes.get("/", authMiddleware, (req, res) =>
+  linkController.findAll(req, res)
+);
+linkRoutes.get("/:id", authMiddleware, (req, res) =>
+  linkController.findById(req, res)
+);
 linkRoutes.get("/user/:userId", authMiddleware, (req, res) =>
   linkController.findByUserId(req, res)
 );
-linkRoutes.put("/:id", authMiddleware, (req, res) => linkController.update(req, res));
-linkRoutes.delete("/:id", authMiddleware, (req, res) => linkController.delete(req, res));
+linkRoutes.put("/:id", authMiddleware, (req, res) =>
+  linkController.update(req, res)
+);
+linkRoutes.delete("/:id", authMiddleware, (req, res) =>
+  linkController.delete(req, res)
+);
 
 export { linkRoutes };
