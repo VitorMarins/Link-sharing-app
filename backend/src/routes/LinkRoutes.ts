a679f9fcf5525fa132750473b7c1147177a948b8
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-04 01:32:47 -0300

    feat(auth): implement authentication middleware for secure routes

--- backend/src/routes/LinkRoutes.ts
+++ backend/src/routes/LinkRoutes.ts
@@ -3,19 +3,20 @@ import { LinkRepository } from "../repositories/LinkRepository";
 import { LinkService } from "../services/LinkService";
 import { LinkController } from "../controllers/LinkController";
 import { userService } from "./UserRoutes";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const linkRoutes: Router = Router();
 const linkRepository = new LinkRepository();
 const linkService = new LinkService(linkRepository, userService);
 const linkController = new LinkController(linkService);
 
-linkRoutes.post("/", (req, res) => linkController.create(req, res));
-linkRoutes.get("/", (req, res) => linkController.findAll(req, res));
-linkRoutes.get("/:id", (req, res) => linkController.findById(req, res));
-linkRoutes.get("/user/:userId", (req, res) =>
+linkRoutes.post("/", authMiddleware, (req, res) => linkController.create(req, res));
+linkRoutes.get("/", authMiddleware, (req, res) => linkController.findAll(req, res));
+linkRoutes.get("/:id", authMiddleware, (req, res) => linkController.findById(req, res));
+linkRoutes.get("/user/:userId", authMiddleware, (req, res) =>
   linkController.findByUserId(req, res)
 );
-linkRoutes.put("/:id", (req, res) => linkController.update(req, res));
-linkRoutes.delete("/:id", (req, res) => linkController.delete(req, res));
+linkRoutes.put("/:id", authMiddleware, (req, res) => linkController.update(req, res));
+linkRoutes.delete("/:id", authMiddleware, (req, res) => linkController.delete(req, res));
 
 export { linkRoutes };
--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -2,16 +2,17 @@ import { Router } from "express";
 import { UserRepository } from "../repositories/UserRepository";
 import { UserService } from "../services/UserService";
 import { UserController } from "../controllers/UserController";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const userRoutes: Router = Router();
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 
-userRoutes.post("/", (req, res) => userController.create(req, res));
-userRoutes.get("/", (req, res) => userController.findAll(req, res));
-userRoutes.get("/:id", (req, res) => userController.findById(req, res));
-userRoutes.put("/:id", (req, res) => userController.update(req, res));
-userRoutes.delete("/:id", (req, res) => userController.delete(req, res));
+userRoutes.post("/", authMiddleware, (req, res) => userController.create(req, res));
+userRoutes.get("/", authMiddleware, (req, res) => userController.findAll(req, res));
+userRoutes.get("/:id", authMiddleware, (req, res) => userController.findById(req, res));
+userRoutes.put("/:id", authMiddleware, (req, res) => userController.update(req, res));
+userRoutes.delete("/:id", authMiddleware, (req, res) => userController.delete(req, res));
 
 export { userRoutes, userService, userRepository };
