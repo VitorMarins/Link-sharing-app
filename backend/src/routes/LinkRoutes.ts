d353a87823d4044c0bc09a4a6c33e27f3f12f395
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-04 01:21:29 -0300

    refactor(LinkRoutes): improve code formatting for user ID route

--- backend/src/routes/LinkRoutes.ts
+++ backend/src/routes/LinkRoutes.ts
@@ -12,7 +12,9 @@ const linkController = new LinkController(linkService);
 linkRoutes.post("/", (req, res) => linkController.create(req, res));
 linkRoutes.get("/", (req, res) => linkController.findAll(req, res));
 linkRoutes.get("/:id", (req, res) => linkController.findById(req, res));
-linkRoutes.get("/user/:userId", (req, res) => linkController.findByUserId(req, res));
+linkRoutes.get("/user/:userId", (req, res) =>
+  linkController.findByUserId(req, res)
+);
 linkRoutes.put("/:id", (req, res) => linkController.update(req, res));
 linkRoutes.delete("/:id", (req, res) => linkController.delete(req, res));
 
