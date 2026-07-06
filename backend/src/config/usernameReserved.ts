10f18232ab6f96f59c65be7c875e3bbe96cbf2b9
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-06 13:26:08 -0300

    feat(config): add reserved usernames list for validation

---
+++ backend/src/config/usernameReserved.ts
@@ -0,0 +1,19 @@
+const usernamesReserved = [
+  "admin",
+  "api",
+  "www",
+  "support",
+  "app",
+  "blog",
+  "mail",
+  "ftp",
+  "smtp",
+  "dns",
+  "ns",
+  "localhost",
+  "example",
+  "test",
+  "invalid",
+];
+
+export default usernamesReserved;
