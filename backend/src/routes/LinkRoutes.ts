a4617560934ad7643ad26f90aaae9ba5a0edbf54
Vitor Marins <vitorbarbosamarins@gmail.com>
2026-07-08 23:30:28 -0300

    fix(LinkRoutes): add missing semicolon in userService instantiation

--- backend/src/routes/LinkRoutes.ts
+++ backend/src/routes/LinkRoutes.ts
@@ -8,7 +8,7 @@ import { authMiddleware } from "../middlewares";
 
 const linkRoutes: Router = Router();
 const userRepository = new UserRepository();
-const userService = new UserService(userRepository)
+const userService = new UserService(userRepository);
 const linkRepository = new LinkRepository();
 const linkService = new LinkService(linkRepository, userService);
 const linkController = new LinkController(linkService);
